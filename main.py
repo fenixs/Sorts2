60387dcd59af6bd1b7db88be15a7fc646313f815
went <fenixwent@hotmail.com>
2017-06-07 18:23:52 +0800

    2nd
    添加统计 累计

--- main.py
+++ main.py
@@ -22,6 +22,13 @@ def myAlign(text,width):
             cnCount += 1
     return ' ' * (width-cnCount-len(utext)) + stext
 
+def getRank(gc1,gc2,gc3,gc4,myScore):
+    '''从四个数据中返回名次'''
+    all = [gc1,gc2,gc3,gc4]
+    all.sort()
+    all.reverse()
+    return str(all.index(myScore) + 1)
+
 #fe:12939|Philip：12881|12569：zzf
 def getData(userid,cnt=1000,starttime='2017-01-01 00:00:00',endtime='2099-01-01 00:00:00'):
     '''用户名,数据条数'''
@@ -54,6 +61,16 @@ def getData(userid,cnt=1000,starttime='2017-01-01 00:00:00',endtime='2099-01-01 
     idname = {}  #id和name
     times = {} #对战次数
     myScores = [] #我的成绩
+    mySumScores = [] #我的累计成绩
+    myScoresVsOther = {}   #面对其他人时候的我的成绩
+    myRanks = {'1':0,'2':0,'3':0,'4':0}
+    isUseDate = False
+    st = datetime.datetime.now()
+    et = datetime.datetime.now()
+    if starttime!='2017-01-01 00:00:00' or endtime!='2099-01-01 00:00:00':
+        isUseDate = True
+
+
     dataFile.write('id,tableid,roomid,starttime,endtime,pantype,userid1,username1,score1,userid2,username2,score2,userid3,username3,score3,userid4,username4,score4 \n')
     for d in datas:
         user1=(d['user_1']).encode("utf-8")     #获取到用户id
@@ -64,14 +81,17 @@ def getData(userid,cnt=1000,starttime='2017-01-01 00:00:00',endtime='2099-01-01 
         gc2= int(d['gamecoin_2'])
         gc3= int(d['gamecoin_3'])
         gc4= int(d['gamecoin_4'])
+        myScore = 0
         if(user1==_userid2):    #记录自己成绩
-            myScores.append(gc1)
+            myScore = gc1
         elif(user2==_userid2):
-            myScores.append(gc2)
+            myScore = gc2
         elif(user3==_userid2):
-            myScores.append(gc3)
+            myScore = gc3
         elif(user4==_userid2):
-            myScores.append(gc4)
+            myScore = gc4
+        myScores.append(myScore)
+        myRanks[getRank(gc1,gc2,gc3,gc4,myScore)] += 1
         username1 = (d['showname_1']).encode("utf-8")   #各个用户名
         username2 = (d['showname_2']).encode("utf-8")
         username3 = (d['showname_3']).encode("utf-8")
@@ -82,27 +102,35 @@ def getData(userid,cnt=1000,starttime='2017-01-01 00:00:00',endtime='2099-01-01 
         if(scores.has_key(user1)):  #保存各用户的成绩和次数
             scores[user1] += gc1
             times[user1] +=1
+            myScoresVsOther[user1] += myScore
         else:
             scores[user1] = gc1
             times[user1]  =1
+            myScoresVsOther[user1] = myScore
         if(scores.has_key(user2)):
             scores[user2] += gc2
             times[user2] +=1
+            myScoresVsOther[user2] += myScore
         else:
             scores[user2] = gc2
             times[user2]  =1
+            myScoresVsOther[user2] = myScore
         if(scores.has_key(user3)):
             scores[user3] += gc3
             times[user3] +=1
+            myScoresVsOther[user3] += myScore
         else:
             scores[user3] = gc3
             times[user3]  =1
+            myScoresVsOther[user3] = myScore
         if(scores.has_key(user4)):
             scores[user4] += gc4
             times[user4] +=1
+            myScoresVsOther[user4] += myScore
         else:
             scores[user4] = gc4
             times[user4]  =1
+            myScoresVsOther[user4] = myScore
         idname.setdefault(user1,username1)
         idname.setdefault(user2,username2)
         idname.setdefault(user3,username3)
@@ -110,12 +138,24 @@ def getData(userid,cnt=1000,starttime='2017-01-01 00:00:00',endtime='2099-01-01 
         cnt+=1
         circle += int(d['pantype'])
 
-    dataFile.write('%30s%7s:%s%s\n'%(' ',' ',myAlign('数据',10),myAlign('次数',10)))
-    for key in scores:
-        dataFile.write('%s(%s):%10s%10s\n'%(myAlign(idname[key],30),key,str(scores[key]),str(times[key])))
+    myScores.reverse()
+    #计算累计成绩
+    lastAllScore = 0
+    for score in myScores:
+        lastAllScore += score
+        mySumScores.append(lastAllScore)
 
-    dataFile.write("my scores:" + ','.join(str(s) for s in myScores) + "\n")
-    dataFile.write('total count:' + str(cnt) + "  total quan:" + str(circle) + "\n")
+
+    dataFile.write('%30s%7s:%s%s%s\n'%(' ',' ',myAlign('数据',10),myAlign('次数',10),myAlign('我的数据',10)))
+    for key in scores:
+        dataFile.write('%s(%s):%10s%10s%10s\n'%(myAlign(idname[key],30),key,str(scores[key]),str(times[key]),str(myScoresVsOther[key])))
+
+    dataFile.write("\nmy scores:" + ','.join(str(s) for s in myScores) + "\n")
+    dataFile.write("\nmy sum scores:" + ','.join(str(s) for s in mySumScores) + "\n")
+    # for s in mySumScores:
+    #     dataFile.write(str(s) + "\n")
+    dataFile.write("\nmy rank: 1st: %s,2nd: %s,3rd: %s,4th: %s \n"%(str(myRanks['1']),str(myRanks['2']),str(myRanks['3']),str(myRanks['4'])))
+    dataFile.write('\ntotal count:' + str(cnt) + "  total quan:" + str(circle) + "\n")
     dataFile.close()
 
 
@@ -139,7 +179,7 @@ def getMultiDatas():
 
 
 #fe:12939|Philip：12881|12569：zzf
-getData(12794)
+getData(12939)
 # getMultiDatas()
 
 
